3952f1aa5d93ce6f31539dc2f212efdde6b7b11f
Naveen Singh <naveen8376singh@gmail.com>
2025-02-12 23:05:40 +0530

    ...

---
+++ functions.js
@@ -0,0 +1,57 @@
+// function myName(name){
+// 	console.log(name)
+// }
+// myName("Naveen Singh")
+
+
+
+
+// const myFunction = function() {
+// 	return "Hello World"
+// 	//after return statement, code function doesnt return anything
+// }
+// console.log(myFunction())
+
+
+
+// const hello = (name) => `Hello My name is ${name}` 
+// console.log(hello("Naveen Junglan"));
+
+
+
+// const hello = (name = "Tinku Junglan") => `Hello My name is ${name}` //defualt value of the the parameter
+// console.log(hello());
+
+
+
+
+//Function with rest operator => ... are used to define rest operator
+// function restFun(num1, ...num2) {
+// 	console.log("Result : ", num2)
+// }
+// restFun(2, 4, 6, 8)
+
+
+
+//function with objects
+// const myOb = {
+// 	name:"naveen",
+// 	age:24
+// }
+
+// function helloOb(o){//first way
+// 	console.log(`User name is ${o.name} and his age is ${o.age}`)
+// }
+// helloOb(myOb)
+
+
+
+// function helloOb(o){//Second approach
+// 	console.log(`User name is ${o.name} and his age is ${o.age}`)
+// }
+// helloOb({
+// 	name : "Tinku Singh",
+// 	age : 22
+// })
+
+
