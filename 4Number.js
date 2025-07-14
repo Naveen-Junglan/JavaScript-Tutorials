cbd35d23ef62593b31f40c80e154253bd2d52c73
Naveen Singh <naveen8376singh@gmail.com>
2025-07-14 13:36:25 +0530

    Math methods in JS

---
+++ 4Number.js
@@ -0,0 +1,24 @@
+const num = new Number(1001234)
+
+// console.log(num.toExponential(2))
+// console.log(num.toPrecision(3))
+// console.log(num.toFixed(5))//put the number of zeros defined into arg after decimal
+
+
+
+
+// -------------------------------------Math Method in JS-------------------------------------
+
+
+// console.log(Math.abs(5.5));//returns absolute (+) value of the number
+// console.log(Math.round(5.9));//returns the value of a number rounded to the nearest integer.
+
+// console.log(Math.sqrt(4))//returns the square root of the number
+// console.log(Math.pow(2, 3))//returns the value of a base raised to a power
+
+// console.log(Math.ceil(4.1))//rounds up and returns the smallest integer greater than or equal to a given number.
+// console.log(Math.floor(4.9))//rounds down and returns the largest integer less than or equal to a given number.
+
+// console.log(Math.max(12, 32, 21, 33, 42, 13, 4, 66))//returns the largest of the numbers
+// console.log(Math.min(12, 32, 21, 33, 42, 13, 4, 66))//returns the smallest of the numbers
+console.log(Math.round((Math.random()*16)));
