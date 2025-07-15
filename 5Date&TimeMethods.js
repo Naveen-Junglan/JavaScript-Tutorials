127e7649f646d2d611e4b7bd9946ca689f9d000b
Naveen Singh <naveen8376singh@gmail.com>
2025-07-15 13:56:04 +0530

    Date & Time Methods in JS

---
+++ 5Date&TimeMethods.js
@@ -0,0 +1,37 @@
+// const date = new Date()
+// console.log(date)
+
+const CreatedDate = new Date()//returns current date time
+
+console.log(CreatedDate);//returns date & time for this date according to local time.
+// console.log(CreatedDate.toLocaleDateString());//(MM/DD/YYYY) - returns the local date for this date according to local time.
+
+// console.log(CreatedDate.toDateString());//(Day Month Date Year) - returns the local date with day of week for this date according to local time.
+
+
+// console.log(CreatedDate.toTimeString());//(HH:mm:second GMT+0530 (India Standard Time))returns the local time with 24hrs for this date according to american time.
+
+// console.log(CreatedDate.toLocaleTimeString());//(2:47:33 PM)returns the local time (Accordinng to IST) for this date according to local time.
+
+
+// console.log(CreatedDate.getDate());//(14) - returns date for this date according to local time.
+
+// console.log(CreatedDate.getMonth());//(6) - returns month for this date according to local time.
+
+// console.log(CreatedDate.getFullYear());//(2025) - returns full year for this date according to local time.
+
+// console.log(`${CreatedDate.getDate()}-${CreatedDate.getMonth()}-${CreatedDate.getFullYear()}`);
+
+// console.log(CreatedDate.getTime());//(1752484832550) - returns time in miliseconds from 1-1-1970 for this date according to local time.
+
+// console.log(CreatedDate.getHours());//(14) - returns hours for this date according to local time.
+
+// console.log(CreatedDate.getMinutes());//(50) - returns minutes for this date according to local time.
+
+// console.log(CreatedDate.getSeconds());//(32) - returns secondes for this date according to local time.
+
+// console.log(CreatedDate.getMilliseconds());//(550) - returns miliseconds for this date according to local time.
+
+// console.log(`${CreatedDate.getHours()}-${CreatedDate.getMinutes()}-${CreatedDate.getSeconds()}-${CreatedDate.getMilliseconds()}`);
+
+// console.log(CreatedDate.getDay());//(1) - returns day of the month for this date according to local time.
